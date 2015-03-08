e2fba1aa3b606f35c1c8f8de6cb193397d9b7e14
NikV <techvoltz@gmail.com>
2015-03-08 11:01:19 -0500

    I have a thing. It's like a plan, but with more greatness

--- hello-dalek.php
+++ hello-dalek.php
@@ -17,9 +17,8 @@ function hello_dalek_get_quotes() {
 	Life depends on change, and renewal. - Original Season 4, Episode 3
 	You want weapons? We're in a library! Books! The best weapons in the world! - Reboot Season 2, Episode 2
 	I am and always will be the optimist. The hoper of far-flung hopes and the dreamer of improbable dreams. - Reboot Season 6, Episode 6";
-// Remember to split it into lines....
+
 	$quotes = explode( "\n", $quotes );
-// Then you begin to make it random ,random , random...
 	return wptexturize( $quotes[ mt_rand( 0, count( $quotes ) - 1 ) ] );
 }
 function hello_dalek() {
